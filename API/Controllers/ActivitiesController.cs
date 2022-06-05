2f12fb0407134137bbb7596129b0bccbb25da031
Osama Micro <33346047+Osamamicro@users.noreply.github.com>
2022-06-05 22:58:32 +0300

    End of Section 2 🎉

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+using Domain;
+using Presistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
